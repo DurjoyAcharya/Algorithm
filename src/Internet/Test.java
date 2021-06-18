9cf626ed6189f1b2e6e6b7d88b6d6ac53c5d2c4e
Durjoy <da-durjoy@outlook.com>
2021-06-18 16:27:16 +0600

    Simple Card Program and Example of Arrays of Objects

--- src/Internet/Test.java
+++ src/Internet/Test.java
@@ -1,7 +1,26 @@
 package Internet;
 
+
+class Card{
+    private int rank;
+    private int suits;
+    public Card(int rank, int suits) {
+        this.rank = rank;
+        this.suits = suits;
+    }
+    @Override
+    public String toString()
+    {
+        String[] str={"Clubs","Diamonds","Hearts","Spades"};
+        String[] ranks={null,"Ace","2","3","4","5","6","7","8","9","10",
+                "Jack","Queen","King"};
+        String s=ranks[this.rank]+" Of "+str[this.suits];
+        return s;
+    }
+}
 public class Test {
     public static void main(String[] args) {
-        System.out.println();
+        Card ThreeOfCard=new Card(11,2);
+        System.out.println(ThreeOfCard);
     }
 }
