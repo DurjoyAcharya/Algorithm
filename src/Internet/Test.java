a1c8a534cc471c8bbfc66a56dc868a6b6ed7c679
Durjoy <da-durjoy@outlook.com>
2021-06-18 17:12:42 +0600

    Some Methods added

--- src/Internet/Test.java
+++ src/Internet/Test.java
@@ -8,6 +8,25 @@ class Card{
         this.rank = rank;
         this.suits = suits;
     }
+    public int CompareTo(Card that)
+    {
+        if (this.suits< that.suits)
+            return -1;
+        if (this.suits> that.suits)
+            return 1;
+        if (this.rank<that.rank)
+            return -1;
+        if (this.rank>that.rank)
+            return 1;
+        return 0;
+    }
+    public int getRank() {
+        return this.rank;
+    }
+    public int getSuits() {
+        return this.suits;
+    }
+
     @Override
     public String toString()
     {
@@ -19,8 +38,29 @@ class Card{
     }
 }
 public class Test {
+    public static void CasinoWar(Card[] cards)
+    {
+        for (int i = 0; i < cards.length; i++) {
+            System.out.println(cards[i]);
+        }
+    }
+    public static int Search(Card[] cards, String target)
+    {
+        for (int i = 0; i < cards.length; i++) {
+            if (cards[i].equals(target))
+                return i;
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        Card ThreeOfCard=new Card(11,2);
-        System.out.println(ThreeOfCard);
+        Card[] cards=new Card[52];
+        int index=0;
+        for (int i = 0; i <=3; i++) {
+            for (int j = 1; j <=13; j++) {
+                cards[index]=new Card(j,i);
+                index++;
+            }
+        }
+        CasinoWar(cards);
     }
 }
