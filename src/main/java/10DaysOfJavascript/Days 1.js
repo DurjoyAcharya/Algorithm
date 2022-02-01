d4edab5a0e64818e3ae50b3bb254bb0fd1906d69
Durjoy <da-durjoy@outlook.com>
2022-02-01 11:52:39 +0600

    day 1 data type

---
+++ src/main/java/10DaysOfJavascript/Days 1.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+
+    const firstDecimal = 4.0;
+
+    const firstString = 'HackerRank ';
+    console.log(firstInteger + (+secondInteger));
+    console.log(firstDecimal + (+secondDecimal));
+    console.log(firstString + secondString);
+
+}
+function main() {
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+
+    performOperation(secondInteger, secondDecimal, secondString);
+}
+main();
