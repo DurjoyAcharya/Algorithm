a39bc44600298846c41962392794df9ced0a399f
DurjoyAcharya <da-durjoy@outlook.com>
2023-02-02 23:23:01 +0600

    Bug fixing

--- src/main/Simulation/SingleServerQueue.java
+++ src/main/Simulation/SingleServerQueue.java
@@ -22,7 +22,7 @@ interface SimulationFunction<T>{
 }
 
 public class SingleServerQueue implements SimulationFunction<Simulation>{
-    private Simulation[] s;
+    private Simulation[] simulations;
     private int total_service_time;
     private int N;//Number Of Customer;
     private double avg_waiting_time;
@@ -35,9 +35,9 @@ public class SingleServerQueue implements SimulationFunction<Simulation>{
     private static final java.util.Scanner sc=new java.util.Scanner(System.in);
     public SingleServerQueue()
     {
-        s=new Simulation[200];//initialize with 200 rand
-        for (int i = 0; i < s.length; i++) {
-            s[i]=new Simulation();
+        simulations=new Simulation[200];//initialize with 200 rand
+        for (int i = 0; i < simulations.length; i++) {
+            simulations[i]=new Simulation();
         }
     }
     public void lunchSimulation()
@@ -47,60 +47,69 @@ public class SingleServerQueue implements SimulationFunction<Simulation>{
         System.out.println("Enter Random Digit For Arrival Time For Every Customer: ");
         for (int i = 1; i < 2; i++) {
             System.out.println("Random Arrival[1]: - ");
-            s[i].customer_no=1;
-            s[i].rd_ar=0;
-            s[i].iat=0;
+            simulations[i].customer_no=1;
+            simulations[i].rd_ar=0;
+            simulations[i].iat=0;
         }
         for (int i = 2; i <= N; i++) {
             System.out.printf("  Random Arrival[%d] ",i);
-            s[i].rd_ar= sc.nextInt();
-            s[i].customer_no=i;
-            if(s[i].rd_ar > 0 && s[i].rd_ar < 126)
-                s[i].iat=1;
-            else if(s[i].rd_ar > 125 && s[i].rd_ar < 251)
-                s[i].iat=2;
-            else if(s[i].rd_ar > 250 && s[i].rd_ar < 376)
-                s[i].iat=3;
-            else if(s[i].rd_ar > 375 && s[i].rd_ar < 501)
-                s[i].iat=4;
-            else if(s[i].rd_ar > 500 && s[i].rd_ar < 626)
-                s[i].iat=5;
-            else if(s[i].rd_ar > 625 && s[i].rd_ar < 751)
-                s[i].iat=6;
-            else if(s[i].rd_ar > 750 && s[i].rd_ar < 876)
-                s[i].iat=7;
-            else if(s[i].rd_ar > 875 && s[i].rd_ar < 1001)
-                s[i].iat=8;
+            simulations[i].rd_ar= sc.nextInt();
+            simulations[i].customer_no=i;
+            if(simulations[i].rd_ar > 0 && simulations[i].rd_ar < 126)
+                simulations[i].iat=1;
+            else if(simulations[i].rd_ar > 125 && simulations[i].rd_ar < 251)
+                simulations[i].iat=2;
+            else if(simulations[i].rd_ar > 250 && simulations[i].rd_ar < 376)
+                simulations[i].iat=3;
+            else if(simulations[i].rd_ar > 375 && simulations[i].rd_ar < 501)
+                simulations[i].iat=4;
+            else if(simulations[i].rd_ar > 500 && simulations[i].rd_ar < 626)
+                simulations[i].iat=5;
+            else if(simulations[i].rd_ar > 625 && simulations[i].rd_ar < 751)
+                simulations[i].iat=6;
+            else if(simulations[i].rd_ar > 750 && simulations[i].rd_ar < 876)
+                simulations[i].iat=7;
+            else if(simulations[i].rd_ar > 875 && simulations[i].rd_ar < 1001)
+                simulations[i].iat=8;
             else{
                 System.out.printf(" Warning!! Please Enter Random Arrival. around 1-1000");
                 return;
             }
         }
-        ScheduleArrivalTime(s,N);
+        ScheduleArrivalTime(simulations,N);
         System.out.println(" Enter random digit for service time for each customer: ");
         for(int i=1;i<2;i++)
-            s[i].service_begin=0;
+            simulations[i].service_begin=0;
         for (int i = 1; i <=N; i++) {
             System.out.printf("  Random Service[%d] ",i);
-            s[i].rd_se=sc.nextInt();
-            if(s[i].rd_se > 0 && s[i].rd_se < 11)
-                s[i].service_time=1;
-            else if(s[i].rd_se > 10 && s[i].rd_se < 31)
-                s[i].service_time=2;
-            else if(s[i].rd_se > 30 && s[i].rd_se < 61)
-                s[i].service_time=3;
-            else if(s[i].rd_se > 60 && s[i].rd_se < 86)
-                s[i].service_time=4;
-            else if(s[i].rd_se > 85 && s[i].rd_se < 96)
-                s[i].service_time=5;
-            else if(s[i].rd_se > 95 && s[i].rd_se < 101)
-                s[i].service_time=6;
+            simulations[i].rd_se=sc.nextInt();
+            if(simulations[i].rd_se > 0 && simulations[i].rd_se < 11)
+                simulations[i].service_time=1;
+            else if(simulations[i].rd_se > 10 && simulations[i].rd_se < 31)
+                simulations[i].service_time=2;
+            else if(simulations[i].rd_se > 30 && simulations[i].rd_se < 61)
+                simulations[i].service_time=3;
+            else if(simulations[i].rd_se > 60 && simulations[i].rd_se < 86)
+                simulations[i].service_time=4;
+            else if(simulations[i].rd_se > 85 && simulations[i].rd_se < 96)
+                simulations[i].service_time=5;
+            else if(simulations[i].rd_se > 95 && simulations[i].rd_se < 101)
+                simulations[i].service_time=6;
             else{
                 System.out.printf(" Warning!! Please Enter Random Service. Around 1-100");
                 return;
             }
-            total_service_time+=s[i].service_time;
+            total_service_time+=simulations[i].service_time;
+
         }
+        //Play With ScheduleServiceTime & CompletionOfService
+        ScheduleServiceTime(simulations,N);
+        CompletionOfService(simulations,N);
+        ScheduleServiceTime(simulations,N);
+        CompletionOfService(simulations,N);
+        //After play with ScheduleServiceTime & CompletionOfService then WaitingAndIdle
+        WaitingAndIdleTime(simulations,N);
+
 
 
     }
