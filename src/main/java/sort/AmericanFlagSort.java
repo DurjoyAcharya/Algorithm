4a22b7cc90b45f6efba4ee2d6daf19e8b5e8ab10
darkdev3v <126149172+darkdev3v@users.noreply.github.com>
2023-03-01 13:59:56 +0600

    need number of buckets

--- src/main/java/sort/AmericanFlagSort.java
+++ src/main/java/sort/AmericanFlagSort.java
@@ -1,6 +1,7 @@
 package sort;
 
 public class AmericanFlagSort {
+    private static final int NUMBER_OF_BUCKETS = 10; // 10 for base 10 numbers
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
